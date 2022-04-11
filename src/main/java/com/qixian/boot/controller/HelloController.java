9eaa02cf975f96e134f2dadede381df25410ffcd
1805784360@qq.com <MXF19991028>
2022-04-11 18:28:19 +0800

    1

--- src/main/java/com/qixian/boot/controller/HelloController.java
+++ src/main/java/com/qixian/boot/controller/HelloController.java
@@ -1,13 +1,34 @@
 package com.qixian.boot.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String handle01(){
+        log.info("请求进来了.....");
         return "hello,Spring Boot 2!";
+    }
+    @GetMapping("/user")
+    public String getUser(){
+        return "hello,getUser!";
+    }
+
+    @PutMapping("/user")
+    public String putUser(){
+        return "hello,putUser!";
+    }
+
+    @PostMapping("/user")
+    public String postUser(){
+        return "hello,postUser!";
+    }
+
+    @DeleteMapping("/user")
+    public String deleteUser(){
+        return "hello,deleteUser!";
     }
 }
